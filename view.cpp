f3913a9ac60ca2a2e95d636a5e5b09c0c906ba8a
andrea.mancuso <andrea.mancuso@u-blox.com>
2024-04-27 15:58:20 +0200

    Update glwasm.cpp and view.cpp

---
+++ view.cpp
@@ -0,0 +1,32 @@
+#include <GLFW/glfw3.h>
+#include <webgpu/webgpu.h>
+#include <webgpu/webgpu_cpp.h>
+
+#ifndef VIEW
+#define VIEW
+
+class View {
+    protected:
+        const char* windowId;
+        const char* glWindowTitle;
+
+    public:
+        View(const char* newWindowId, const char* newGlWindowTitle) {
+            windowId = newGlWindowTitle;
+            glWindowTitle = newGlWindowTitle;
+        }
+
+        const char* GetGlWindowTitle() {
+            return glWindowTitle;
+        }
+
+        virtual void SetUp(std::string canvasSelector, WGPUDevice device, GLFWwindow* glfwWindow, WGPUTextureFormat wgpu_preferred_fmt) = 0;
+        virtual void PrepareForRender() = 0;
+        virtual void Render(int window_width, int window_height) = 0;
+        virtual void RenderDrawData(WGPURenderPassEncoder pass) = 0;
+        virtual void CleanUp() = 0;
+        virtual void HandleScreenSizeChanged() = 0;
+        virtual WGPUColor GetClearColor() = 0;
+};
+
+#endif
